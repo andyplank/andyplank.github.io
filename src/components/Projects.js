5cd966a31f0f0884da57e40d21883706b610c728
Andrew Plank <aplank14@gmail.com>
2020-08-26 23:19:15 -0400

    Fixed css import

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Jumbotron from 'react-bootstrap/Jumbotron'
 
 const Projects = () => {
@@ -10,9 +9,6 @@ const Projects = () => {
       <p>
         This is a simple hero unit, a simple jumbotron-style component for calling
         extra attention to featured content or information.
-      </p>
-      <p>
-        <Button variant="primary">Learn more</Button>
       </p>
     </Jumbotron>
     </div>
