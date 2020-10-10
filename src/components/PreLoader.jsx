4004b5c1c144e1172e318874ae82e63cfa20238d
Aplank14 <aplank14@gmail.com>
2020-10-10 13:21:07 -0400

    Added fade in text

--- src/components/PreLoader.jsx
+++ src/components/PreLoader.jsx
@@ -3,16 +3,23 @@ import './PreLoader.css';
 
 const PreLoader = () => {
     const [loading, setLoading] = useState(true);
+    const [fading, setFading] = useState(true);
+
+    setTimeout(() => {
+        setFading(false)
+    }, 1500);
 
     setTimeout(() => {
         setLoading(false);
-      }, 2000
+      }, 2500  
     );
 
     return ( 
         <div className={loading ? 'loader_bg' : 'loader_bg hide'}>
             <div className="w-100 h-100 d-flex align-items-center justify-content-center">
-                <h2>Hello</h2>
+            <div className={fading ? 'fade-in' : 'fade-in hide'}>
+                <h1>Hello.</h1>
+            </div>
             </div>
         </div>
     );
